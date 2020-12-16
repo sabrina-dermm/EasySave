b68b659fe4a965ea5443f4383e64b3515abd06f8
sabrina <sabrina.dermeche.dz@cesi.fr>
2020-12-16 01:08:48 +0100

    mono instence

--- EasySaveV2/EasySaveV2/App.xaml.cs
+++ EasySaveV2/EasySaveV2/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SingleInstanceCore;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -11,7 +12,27 @@ namespace EasySaveV2
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : Application, ISingleInstance
     {
-    }
+        public void OnInstanceInvoked(string[] args)
+        {
+            //What to do with the args another instance has sent
+        }
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			bool isFirstInstance = SingleInstance<App>.InitializeAsFirstInstance("EasySaveV2");
+			if (!isFirstInstance)
+			{
+				//If it's not the first instance, arguments are automatically passed to the first instance
+				//OnInstanceInvoked will be raised on the first instance
+				//You may shut down the current instance
+				Current.Shutdown();
+			}
+		}
+
+		private void Application_Exit(object sender, ExitEventArgs e)
+		{
+			SingleInstance<App>.Cleanup();
+		}
+	}
 }
